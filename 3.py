49e0e695cd6d2df311e28c62a225dd69cbdd43db
near-you-luV <v.pelishenko1999@gmail.com>
2025-07-03 20:49:00 +0300

    fix

--- 3.py
+++ 3.py
@@ -4,25 +4,24 @@ from reviews import scrape_reviews
 from logger import logger
 from scrape_apps import fetch_sitemap
 from threading import Thread
-BASE_URL = 'https://apps.shopify.com/categories/store-management-operations-analytics/all'
-def get_app_ids():
-    for page in range(1,37):
-        url = f"{BASE_URL}?page={page}"
-        response = requests.get(url)
-        if response.status_code != 200:
-            logger.error(f"Failed to fetch page {page}, trying again...")
-            while response.status_code != 200:
+# def get_app_ids():
+#     for page in range(1,37):
+#         url = f"{BASE_URL}?page={page}"
+#         response = requests.get(url)
+#         if response.status_code != 200:
+#             logger.error(f"Failed to fetch page {page}, trying again...")
+#             while response.status_code != 200:
 
-                response = requests.get(url)
-                if response.status_code == 404:
-                    break
-        logger.info(f"Fetching page {page}")
-        soup = BeautifulSoup(response.text, 'html.parser')
-        app_links = soup.find_all("div", attrs={"data-controller": "app-card"})
-        for link in app_links:
-            app_id = link.find('a').get('href').split('/')[-1].split('?')[0]
-            scrape_reviews(app_id)
-            logger.info(f"Scraped reviews for app ID: {app_id}")
+#                 response = requests.get(url)
+#                 if response.status_code == 404:
+#                     break
+#         logger.info(f"Fetching page {page}")
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         app_links = soup.find_all("div", attrs={"data-controller": "app-card"})
+#         for link in app_links:
+#             app_id = link.find('a').get('href').split('/')[-1].split('?')[0]
+#             scrape_reviews(app_id)
+#             logger.info(f"Scraped reviews for app ID: {app_id}")
 
 def scrape_app_reviews(app_ids=[]):
     print(urls[:10])
@@ -32,7 +31,7 @@ def scrape_app_reviews(app_ids=[]):
 
 if __name__ == "__main__":
     urls = fetch_sitemap()
-    [url.split('/')[-1].split('?')[0] for url in urls]
+    [url.split('/')[-1].split('?')[0] for url in urls if 'partners' not in url]
     num_threads = 4
     threads = []
     batch_size = len(urls) // num_threads
